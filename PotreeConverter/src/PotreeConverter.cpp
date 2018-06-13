5d035c8b44698435a85188d759d2148337dd8357
Jonne Davidsson <jonne@tripoint.fi>
2018-06-13 11:56:39 +0200

    Indicate where AABB is from

--- PotreeConverter/src/PotreeConverter.cpp
+++ PotreeConverter/src/PotreeConverter.cpp
@@ -93,8 +93,8 @@ void PotreeConverter::prepare(){
 				fs::path pDirectoryEntry = it->path();
 				if(fs::is_regular_file(pDirectoryEntry)){
 					string filepath = pDirectoryEntry.string();
-					if(iEndsWith(filepath, ".las") 
-						|| iEndsWith(filepath, ".laz") 
+					if(iEndsWith(filepath, ".las")
+						|| iEndsWith(filepath, ".laz")
 						|| iEndsWith(filepath, ".xyz")
 						|| iEndsWith(filepath, ".pts")
 						|| iEndsWith(filepath, ".ptx")
@@ -127,14 +127,16 @@ void PotreeConverter::prepare(){
 AABB PotreeConverter::calculateAABB(){
 	AABB aabb;
 	if(aabbValues.size() == 6){
+		std::cout << "PotreeConverter::calculateAABB: Using AABB from args" << endl;
 		Vector3<double> userMin(aabbValues[0],aabbValues[1],aabbValues[2]);
 		Vector3<double> userMax(aabbValues[3],aabbValues[4],aabbValues[5]);
 		aabb = AABB(userMin, userMax);
 	}else{
+		std::cout << "PotreeConverter::calculateAABB: Using AABB from files" << endl;
 		for(string source : sources){
 
 			PointReader *reader = createPointReader(source, pointAttributes);
-			
+
 			AABB lAABB = reader->getAABB();
 			aabb.update(lAABB.min);
 			aabb.update(lAABB.max);
@@ -196,7 +198,7 @@ void PotreeConverter::generatePage(string name){
 				}else{
 					out << "\t\t" << "viewer.setBackground(\"gradient\"); // [\"skybox\", \"gradient\", \"black\", \"white\"];\n";
 				}
-				
+
 				string descriptionEscaped = string(description);
 				std::replace(descriptionEscaped.begin(), descriptionEscaped.end(), '`', '\'');
 
@@ -204,7 +206,7 @@ void PotreeConverter::generatePage(string name){
 			}else{
 				out << line << endl;
 			}
-			
+
 		}
 
 		in.close();
@@ -212,7 +214,7 @@ void PotreeConverter::generatePage(string name){
 	}
 
 	// change lasmap template
-	if(!this->projection.empty()){ 
+	if(!this->projection.empty()){
 		ifstream in( mapTemplateSourcePath );
 		ofstream out( mapTemplateTargetPath );
 
@@ -223,7 +225,7 @@ void PotreeConverter::generatePage(string name){
 			}else{
 				out << line << endl;
 			}
-			
+
 		}
 
 		in.close();
@@ -279,8 +281,8 @@ void writeSources(string path, vector<string> sourceFilenames, vector<int> numPo
 		Value jBounds(rapidjson::kObjectType);
 
 		{
-			Value bbMin(rapidjson::kObjectType);	
-			Value bbMax(rapidjson::kObjectType);	
+			Value bbMin(rapidjson::kObjectType);
+			Value bbMax(rapidjson::kObjectType);
 
 			bbMin.SetArray();
 			bbMin.PushBack(boundingBox.min.x, d.GetAllocator());
@@ -305,8 +307,8 @@ void writeSources(string path, vector<string> sourceFilenames, vector<int> numPo
 
 	Value jBoundingBox(rapidjson::kObjectType);
 	{
-		Value bbMin(rapidjson::kObjectType);	
-		Value bbMax(rapidjson::kObjectType);	
+		Value bbMin(rapidjson::kObjectType);
+		Value bbMax(rapidjson::kObjectType);
 
 		bbMin.SetArray();
 		bbMin.PushBack(bb.min.x, d.GetAllocator());
@@ -330,7 +332,7 @@ void writeSources(string path, vector<string> sourceFilenames, vector<int> numPo
 	//PrettyWriter<StringBuffer> writer(buffer);
 	Writer<StringBuffer> writer(buffer);
 	d.Accept(writer);
-	
+
 	if(!fs::exists(fs::path(path))){
 		fs::path pcdir(path);
 		fs::create_directories(pcdir);
@@ -439,15 +441,15 @@ void PotreeConverter::convert(){
 			}
 			if((pointsProcessed % (10'000'000)) == 0){
 				cout << "FLUSHING: ";
-			
+
 				auto start = high_resolution_clock::now();
-			
+
 				writer->flush();
-			
+
 				auto end = high_resolution_clock::now();
 				long long duration = duration_cast<milliseconds>(end-start).count();
 				float seconds = duration / 1'000.0f;
-			
+
 				cout << seconds << "s" << endl;
 			}
 
@@ -458,9 +460,9 @@ void PotreeConverter::convert(){
 		reader->close();
 		delete reader;
 
-		
+
 	}
-	
+
 	cout << "closing writer" << endl;
 	writer->flush();
 	writer->close();
@@ -473,7 +475,7 @@ void PotreeConverter::convert(){
 	auto end = high_resolution_clock::now();
 	long long duration = duration_cast<milliseconds>(end-start).count();
 
-	
+
 	cout << endl;
 	cout << "conversion finished" << endl;
 	cout << pointsProcessed << " points were processed and " << writer->numAccepted << " points ( " << percent << "% ) were written to the output. " << endl;
